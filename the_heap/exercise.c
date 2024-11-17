b7dd7540258e05ccc5c357f683034b221b5bd7a3
Emerson <emerson.day@gmail.com>
2024-11-17 09:31:26 -0800

    the heap

---
+++ the_heap/exercise.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "exercise.h"
+
+char* get_full_greeting(char *greeting, char *name, int size) {
+  // char full_greeting[100];
+  char *full_greeting = (char*)malloc(size * sizeof(char));
+  snprintf(full_greeting, size, "%s %s", greeting, name);
+  return full_greeting;
+}
